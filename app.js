4a13b033ffe40ae8f159ddcd4848e3f0463aa9de
jakariya0dev <jhalhadi@gmail.com>
2025-06-16 16:14:52 +0600

    some minor issue fixed

--- app.js
+++ app.js
@@ -54,10 +54,12 @@ const client = new MongoClient(process.env.MONGODB_URI, {
 
 async function run() {
   try {
+    // Connect the to the server (optional)
     await client.connect();
     await client.db(process.env.DB_NAME).command({ ping: 1 });
     console.log("Pinged to your MongoDB!");
 
+    // Collections
     const roomCollections = client.db(process.env.DB_NAME).collection("rooms");
     const reviewCollections = client
       .db(process.env.DB_NAME)
@@ -376,14 +378,6 @@ async function run() {
   }
 }
 run().catch(console.dir);
-
-// app.post("/api/rooms", async (req, res) => {
-//   const newRoom = req.body;
-//   const result = await roomsCollection.insertOne(newRoom);
-//   res.status(201).json(result);
-// });
-
-// app.get("/", (req, res) => res.send("Hotel Booking Server is Running"));
 
 app.listen(PORT, () => {
   console.log(`Server running at ${PORT}`);
